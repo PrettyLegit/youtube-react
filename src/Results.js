415946cbc2a3e12164b57bb024a1c0c0e79dffd1
Jimmy Nguyen <nvjimmy27@gmail.com>
2021-06-06 18:26:14 -0400

    Updates

--- src/Results.js
+++ src/Results.js
@@ -2,22 +2,22 @@ import React from "react";
 import Video from "./Video";
 
 const Results = ({ videos }) => {
-    return (
-        <div className="search-result">
-            {videos.map((video) => {
-                return (
-                    <Video
-                        key = {video.id.videoId}
-                        title = {video.snippet.title}
-                        dateAdded = {video.snippet.publishedAt}
-                        channel = {video.snippet.channelTitle}
-                        
-                    />
-                )
-            })}
-        </div>
-    )
-}
+  return (
+    <div className="search-result">
+      {videos.map((video) => {
+        return (
+          <Video
+            key={video.id.videoId}
+            title={video.snippet.title}
+            dateAdded={video.snippet.publishedAt}
+            thumbnails={video.snippet.thumbnail.medium}
+            channel={video.snippet.channelTitle}
+            description={video.snippet.description}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export default Results;
-//thumbnails = {video.snippet.thumbnails.medium}
